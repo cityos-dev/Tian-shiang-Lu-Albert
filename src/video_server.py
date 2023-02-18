b3f4e94c28b409f36ee3eb4b69180675b0fc2ba4
morriship <morriship.bw@gmail.com>
2023-02-18 12:56:20 +0800

    Implement entrypoint

---
+++ src/video_server.py
@@ -0,0 +1,18 @@
+from flask import Flask
+from pymongo import MongoClient
+
+from api.http import health, files
+
+app = Flask(__name__)
+
+
+def create_video_server():
+    app = Flask(__name__)
+    app.register_blueprint(health.routes)
+    app.register_blueprint(files.routes)
+    return app
+
+
+if __name__ == '__main__':
+    app = create_video_server()
+    app.run(port=8080)
